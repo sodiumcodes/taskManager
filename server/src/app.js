068802e49d7e31253478b64dac39078e2363e21a
Manash2005 <swainm099@gmail.com>
2026-02-25 16:49:52 +0530

    user can login and register

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const postRouter = require("./routes/post.routes")
+const authRouter = require("./routes/auth.routes")
 const app = express();
 
 //Built in middlewares
@@ -8,5 +9,6 @@ app.use(express.urlencoded());
 
 
 app.use('/task', postRouter)
+app.use('/auth', authRouter)
 
 module.exports = app;
