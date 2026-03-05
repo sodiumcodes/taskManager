0cf12505f5005f94eb2ecf14d03759cd747a7c73
Manash2005 <swainm099@gmail.com>
2026-03-05 16:48:03 +0530

    fix : return response for uploading image

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -2,27 +2,32 @@ const client = require('../../config/imagekit');
 const userModel = require('../../models/user.model');
 const uploadPfp = async (req, res) => {
     try {
-        async function uploadFile(buffer){
+        async function uploadFile(buffer) {
             const response = await client.files.upload({
                 file: buffer.toString('base64'),
                 fileName: 'image.jpg',
                 folder: "/taskManager/users"
             });
-        return response;
+            return response;
         }
         const image = await uploadFile(req.file.buffer);
 
-        userModel.findOneAndUpdate({
-            user:req.user.id
-        },{
+        const user = await userModel.findOneAndUpdate({
+            _id: req.user.id
+        }, {
             profile_picture: image.url,
         })
         return res.status(201).json({
-            message: "picture uploaded successfully"
+            message: "picture uploaded successfully",
+            user
         })
 
     } catch (error) {
         console.log("Image not uploaded.\n", error);
+        return res.status(500).json({
+            message: "Image not uploaded",
+            error
+        })
     }
 }
-module.exports= uploadPfp;
+module.exports = uploadPfp;
