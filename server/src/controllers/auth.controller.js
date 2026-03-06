d138bc04ec91459a491b7a7f6b6dd07d3320a33d
Manash2005 <swainm099@gmail.com>
2026-03-06 11:08:26 +0530

    feature : new route auth/me and default profile picture

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,36 +2,36 @@ const userModel = require("../../models/user.model")
 const bcrypt = require("bcrypt")
 const jwt = require("../../config/jwt")
 
-async function register(req, res){
+async function register(req, res) {
 
-    try{
+    try {
 
         //Collect data
-        const {name, email, password} = req.body;
-        
+        const { name, email, password } = req.body;
+
         //Validate data
-        if(!name || !email || !password){
+        if (!name || !email || !password) {
             return res.status(400).json({
                 message: "All fields are required"
             })
         }
-        
+
         //Check if user already exists
-        const existingUser = await userModel.findOne({email});
-        if(existingUser){
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
             return res.status(400).json({
                 message: "User already exists"
             })
         }
-        
+
         //Hash password
         const hashedPassword = await bcrypt.hash(password, 10);
-        
+
         //Create user
         const user = await userModel.create({
             name, email, password: hashedPassword
         });
-        
+
         //send response
         res.status(201).json({
             message: "User registered",
@@ -43,24 +43,24 @@ async function register(req, res){
             error: err.message
         });
     }
-        
+
 }
 
-async function login(req, res){
+async function login(req, res) {
     try {
         //Collect data
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
         //Validate data
-        if(!email || !password){
+        if (!email || !password) {
             return res.status(400).json({
                 message: "All fields are required"
             })
         }
 
         //Check if user exists
-        const user = await userModel.findOne({email});
-        if(!user){
+        const user = await userModel.findOne({ email });
+        if (!user) {
             return res.status(400).json({
                 message: "Invalid credentials"
             })
@@ -68,16 +68,16 @@ async function login(req, res){
 
         //Compare password
         const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch){
+        if (!isMatch) {
             return res.status(400).json({
                 message: "Invalid credentials"
             })
         }
 
         //Generate token
-        const token = jwt.generateToken({id: user._id, email: user.email});
+        const token = jwt.generateToken({ id: user._id, email: user.email });
         res.cookie("token", token);
-        
+
         // also include token in body so front end can store/use it
         res.status(200).json({
             message: "Login successful",
@@ -93,7 +93,22 @@ async function login(req, res){
     }
 }
 
+function me(req, res) {
+    try {
+        res.status(200).json({
+            message: "User fetched successfully",
+            user: req.user
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error fetching user",
+            error: error.message
+        })
+    }
+}
+
 module.exports = {
     register,
-    login
+    login,
+    me
 }
